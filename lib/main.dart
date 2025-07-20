d9000217aa474cf9d20bcb5aae387312af9f4e38
ramyaminn <ramy.amin@carinawear.com>
2025-07-20 11:20:21 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _DashboardPageState extends State<DashboardPage> {
       message = '';
     });
 
-    const token = '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';
+    // const token = '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';
     const repo = 'ramyaminn/config-cluewear-com';
     const branch = 'main';
 
